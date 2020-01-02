45efa71c792e9d2206e54f2b7dc7bb692b5328c3
Tona Diaz <drivexcite@gmail.com>
2020-01-02 14:54:48 -0700

    Added Azure Functions project and Remote Container Configuration for .NET Core 3.1

---
+++ Health.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionsOnDocker
+{
+    public static class Health
+    {
+        [FunctionName("Health")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult(new { healthy = true });
+        }
+    }
+}
